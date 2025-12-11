db9176857b89bca19b744cf8fcd9f568ff7a77b4
subham4849 <wateryblue88904@gmail.com>
2025-12-11 20:48:06 +0530

    adding the db connection

--- backend/db.php
+++ backend/db.php
@@ -1,12 +1,12 @@
 <?php
-$host = getenv("DB_HOST");
-$db   = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASSWORD");
+$host = "127.0.0.1:3307";
+$user = "root";
+$pass = "admin123";
+$dbname = "gurukul_db";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $dbname);
 
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("❌ Database connection failed: " . mysqli_connect_error());
 }
 ?>
